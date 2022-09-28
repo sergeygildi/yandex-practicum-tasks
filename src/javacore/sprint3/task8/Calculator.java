b164c2344730df3bd06a9bdc545ac53a4ce8ebd9
serhiihildi <sergeygildi@gmail.com>
2022-09-28 13:36:51 +0300

    OOP. Task 8. Solution.

---
+++ src/javacore/sprint3/task8/Calculator.java
@@ -0,0 +1,23 @@
+package javacore.sprint3.task8;
+
+import java.util.List;
+
+public class Calculator {
+
+    /**
+     * @param mediaItems возвращает общее количество дней, потраченных на просмотр фильмов и сериалов
+     * @return
+     */
+    public static double calculate(List<MediaItem> mediaItems) {
+        double minutes = 0;
+        for (MediaItem mediaItem : mediaItems) {
+            if (mediaItem instanceof Movie) {
+                minutes += mediaItem.getRuntime();
+            } else if (mediaItem instanceof Series) {
+                minutes += mediaItem.getRuntime() * ((Series) mediaItem).getSeriesCount();
+            }
+        }
+        return minutes / (60*24);
+    }
+
+}
---
+++ src/javacore/sprint3/task8/MediaItem.java
@@ -0,0 +1,9 @@
+package javacore.sprint3.task8;
+
+public interface MediaItem {
+
+    public int getRuntime();
+
+    public String getTitle();
+
+}
