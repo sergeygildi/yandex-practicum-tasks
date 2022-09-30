6b71e38ea316d7d6b2006f50eb997fc57bc88e2f
serhiihildi <sergeygildi@gmail.com>
2022-09-30 13:01:51 +0300

    Algorithms. Task 3. Solution.

---
+++ src/javacore/sprint4/task3/Main.java
@@ -0,0 +1,50 @@
+package javacore.sprint4.task3;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array1 = new int[]{1, 3, 4, 6, 8};
+        int[] array2 = new int[]{1, 2, 3, 8, 9};
+        int[] resultArray = merge(array1, array2);
+
+        for (int e : resultArray) {
+            System.out.print(e + ", ");
+        }
+    }
+
+    private static int[] merge(int[] leftArray, int[] rightArray) {
+
+        int leftSize = leftArray.length;
+        int rightSize = rightArray.length;
+        int[] resultArray = new int[leftSize + rightSize];
+
+        int left = 0;
+        int right = 0;
+        int index = 0;
+
+        while (index < resultArray.length) {
+
+            if (left >= leftSize) {
+                resultArray[index] = rightArray[right];
+                right++;
+            }
+
+            else if (right >= rightSize) {
+                resultArray[index] = leftArray[right];
+                left++;
+            }
+
+            else if (leftArray[left] <= rightArray[right]) {
+                resultArray[index] = leftArray[left];
+                left++;
+            }
+            else {
+                resultArray[index] = rightArray[right];
+                right++;
+            }
+            index++;
+        }
+
+        return resultArray;
+    }
+
+}
