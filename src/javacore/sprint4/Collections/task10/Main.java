35cdb1232498b8901867641abc553092b93a35eb
serhiihildi <sergeygildi@gmail.com>
2022-10-15 18:16:29 +0300

    Collections. Task 10. Solution.

---
+++ src/javacore/sprint4/Collections/task10/Main.java
@@ -0,0 +1,55 @@
+package javacore.sprint4.Collections.task10;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+class Main {
+    private static Map<Long, User> users = new HashMap<>();
+
+    public static void main(String[] args) {
+        for (long i = 1; i <= 1_000_000L; i++) {
+            users.put(i, new User(i, "Имя " + i));
+        }
+
+        final long startTime = System.nanoTime();
+        User user = findUser(378_366L);
+        final long endTime = System.nanoTime();
+
+        System.out.println("Найден пользователь: " + user);
+        System.out.println("Поиск занял " + (endTime - startTime) + " наносекунд.");
+    }
+
+    private static User findUser(Long userId) {
+        return users.getOrDefault(userId, null);
+    }
+
+    static class User {
+        Long id;
+        String name;
+
+        public User(Long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            User user = (User) o;
+            return Objects.equals(id, user.id) && Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name);
+        }
+
+        @Override
+        public String toString() {
+            return "User{id=" + id + ", name='" + name + "'}";
+        }
+    }
+
+}
